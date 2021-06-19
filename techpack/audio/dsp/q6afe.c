a3c299bc33dc4fce28b6b00b1bae8acc4c87c564
Edwin Moquete <edwinmmoquete@gmail.com>
2021-06-19 15:40:24 +0000

    techpack/audio/dsp/q6afe: Don't send unsupported A2DP commands to DSP

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -5131,6 +5131,7 @@ static int q6afe_send_enc_config(u16 port_id,
 		}
 	}
 
+#ifndef CONFIG_ARCH_SDM660
 	if (format == ASM_MEDIA_FMT_APTX) {
 		pr_debug("%s: sending AFE_PARAM_ID_APTX_SYNC_MODE to DSP",
 			__func__);
@@ -5186,6 +5187,7 @@ static int q6afe_send_enc_config(u16 port_id,
 			goto exit;
 		}
 	}
+#endif
 
 	pr_debug("%s:sending AFE_ENCODER_PARAM_ID_PACKETIZER to DSP\n",
 		 __func__);
@@ -5202,6 +5204,7 @@ static int q6afe_send_enc_config(u16 port_id,
 		goto exit;
 	}
 
+#ifndef CONFIG_ARCH_SDM660
 	if (format != ASM_MEDIA_FMT_APTX_AD_SPEECH) {
 		pr_debug("%s:sending AFE_ENCODER_PARAM_ID_ENABLE_SCRAMBLING mode= %d to DSP payload\n",
 			  __func__, scrambler_mode);
@@ -5220,6 +5223,7 @@ static int q6afe_send_enc_config(u16 port_id,
 #endif
 		}
 	}
+#endif
 
 	if (format == ASM_MEDIA_FMT_APTX) {
 		pr_debug("%s:sending CAPI_V2_PARAM_ID_APTX_ENC_SWITCH_TO_MONO mode= %d to DSP payload\n",
