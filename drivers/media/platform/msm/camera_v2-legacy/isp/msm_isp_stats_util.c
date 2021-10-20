b792f90355c00c819d2f73858a9b7cd27fdfb963
Zhijun He <zhijunhe@google.com>
2021-10-20 12:07:16 +0530

    camera_v2-legacy: Properly initialize vfe
    Bug: 62090198

--- drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp_stats_util.c
+++ drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp_stats_util.c
@@ -549,7 +549,7 @@ int msm_isp_release_stats_stream(struct vfe_device *vfe_dev, void *arg)
 	}
 
 	stream_info->num_isp--;
-	stream_info->vfe_dev[stream_info->num_isp] = 0;
+	stream_info->vfe_dev[stream_info->num_isp] = NULL;
 	stream_info->stream_handle[stream_info->num_isp] = 0;
 	stream_info->buffer_offset[stream_info->num_isp] = 0;
 	stream_info->vfe_mask &= ~(1 << vfe_dev->pdev->id);
