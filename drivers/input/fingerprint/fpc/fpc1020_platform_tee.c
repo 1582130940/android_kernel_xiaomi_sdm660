1211d4b960b027f8ba75e3da2e0f71e777f3f3e4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-03-20 15:31:37 +0800

    fpc: Always inline latency critical functions
    There are hot-paths that need their execution to be as fast as possible.
    Therefore, mark the latency critical functions __always_inline.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
@@ -654,7 +654,7 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
-static inline irqreturn_t fpc1020_irq_handler(int irq, void *handle)
+static irqreturn_t __always_inline fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
 
@@ -700,7 +700,7 @@ static inline int fpc1020_request_named_gpio(struct fpc1020_data *fpc1020,
 	return 0;
 }
 
-static inline int fpc_fb_notif_callback(struct notifier_block *nb,
+static int __always_inline fpc_fb_notif_callback(struct notifier_block *nb,
 								 unsigned long val, void *data)
 {
 	struct fpc1020_data *fpc1020 = container_of(nb, struct fpc1020_data,
