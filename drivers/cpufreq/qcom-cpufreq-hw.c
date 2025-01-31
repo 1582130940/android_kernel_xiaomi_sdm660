d39d238ca2d7911b045da59e71c53611162c5526
Amit Kucheria <amit.kucheria@linaro.org>
2025-01-31 19:58:02 +0800

    cpufreq/qcom-cpufreq-hw: Move to device_initcall
    subsys_initcall causes problems registering the driver as a thermal
    cooling device.
    
    If "faster boot" is the main reason for doing subsys_initcall, this
    should be handled in the bootloader or another boot constraint
    framework.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -876,6 +876,6 @@ static int __init qcom_cpufreq_hw_init(void)
 {
 	return platform_driver_register(&qcom_cpufreq_hw_driver);
 }
-subsys_initcall(qcom_cpufreq_hw_init);
+device_initcall(qcom_cpufreq_hw_init);
 
 MODULE_DESCRIPTION("QCOM firmware-based CPU Frequency driver");
