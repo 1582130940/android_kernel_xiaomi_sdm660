80896dd11895bf0acf6cba3a951e208bbb87db54
Alex Finhart <alexfinhart@gmail.com>
2025-02-24 21:17:34 +0800

    kernel/sched/core: Report max frequency to unity tasks
    * More inlining with sunfish
    * Reference: https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-s-beta-1/init.hardware.rc#145
    
    [DeadlyShroud]:
    * Add some popular game packages to sched_lib_name
    
    [johnmart19]:
    * Add Gryphline games to unity boosting
    
    Signed-off-by: Alex Finhart <alexfinhart@gmail.com>
    Change-Id: I4b1b9b6c3073738259f4511ff002abe0b75b8ebb
    Co-Authored-By: DeadlyShroud <prateekgurjar786@protonmail.com>
    Co-Authored-By: johnmart19 <johnivan19999@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5797,8 +5797,8 @@ out_put_task:
 }
 EXPORT_SYMBOL_GPL(sched_setaffinity);
 
-char sched_lib_name[LIB_PATH_LENGTH];
-unsigned int sched_lib_mask_force;
+char sched_lib_name[LIB_PATH_LENGTH] = { "UnityMain,libunity.so,libil2cpp.so,com.miHoYo.,com.pubg.imobile,com.pubg.,com.activision.,com.gryphline."};
+unsigned int sched_lib_mask_force = 255;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
