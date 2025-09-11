562105fc19ba9336ff4683a2aac36ad8f047b9b1
fangqinwu <fangqinwu@xiaomi.com>
2025-09-11 09:31:46 +0800

    fs/proc/task_mmu: Some optimization for reclaim
    BSPSYS-10620
    
    This change includes:
     - Fix reclaim count calculation issue
     - Only reclaim un-shared pages
     - Avoid burning CPU when nr_to_reclaim pages
    
    Change-Id: I04e77a2811f5ac4cae6d261c3eaaf88e26eee2ce
    Signed-off-by: fangqinwu <fangqinwu@xiaomi.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1765,6 +1765,12 @@ cont:
 		if (!page)
 			continue;
 
+		if (!PageLRU(page))
+			continue;
+
+		if (page_mapcount(page) != 1)
+			continue;
+
 		if (isolate_lru_page(compound_head(page)))
 			continue;
 
@@ -1799,7 +1805,7 @@ cont:
 		goto cont;
 
 	cond_resched();
-	return 0;
+	return (rp->nr_to_reclaim == 0) ? -EPIPE : 0;
 }
 
 enum reclaim_type {
@@ -1889,6 +1895,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	unsigned long start = 0;
 	unsigned long end = 0;
 	struct reclaim_param rp;
+	int ret;
 
 	memset(buffer, 0, sizeof(buffer));
 	if (count > sizeof(buffer) - 1)
@@ -1965,9 +1972,11 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			walk_page_range(max(vma->vm_start, start),
+			ret = walk_page_range(max(vma->vm_start, start),
 					min(vma->vm_end, end),
 					&reclaim_walk);
+			if (ret)
+				break;
 			vma = vma->vm_next;
 		}
 	} else {
@@ -1982,8 +1991,10 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			walk_page_range(vma->vm_start, vma->vm_end,
+			ret = walk_page_range(vma->vm_start, vma->vm_end,
 				&reclaim_walk);
+			if (ret)
+				break;
 		}
 	}
 
