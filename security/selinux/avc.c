cf71dfc957fdbc564175245322409d6956d4079b
Zhen Lei <thunder.leizhen@huawei.com>
2026-02-04 17:01:53 +0800

    security/selinux/avc: Simplify avc_xperms_audit_required()
    By associative and commutative laws, the result of the two 'audited' is
    zero. Take the second 'audited' as an example:
      1) audited = requested & avd->auditallow;
      2) audited &= ~requested;
      ==> audited = ~requested & (requested & avd->auditallow);
      ==> audited = (~requested & requested) & avd->auditallow;
      ==> audited = 0 & avd->auditallow;
      ==> audited = 0;
    
    In fact, it is more readable to directly write zero. The value of the
    first 'audited' is 0 because AUDIT is not allowed. The second 'audited'
    is zero because there is no AUDITALLOW permission.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -467,7 +467,7 @@ static inline u32 avc_xperms_audit_required(u32 requested,
 		audited = denied & avd->auditdeny;
 		if (audited && xpd) {
 			if (avc_xperms_has_perm(xpd, perm, XPERMS_DONTAUDIT))
-				audited &= ~requested;
+				audited = 0;
 		}
 	} else if (result) {
 		audited = denied = requested;
@@ -475,7 +475,7 @@ static inline u32 avc_xperms_audit_required(u32 requested,
 		audited = requested & avd->auditallow;
 		if (audited && xpd) {
 			if (!avc_xperms_has_perm(xpd, perm, XPERMS_AUDITALLOW))
-				audited &= ~requested;
+				audited = 0;
 		}
 	}
 
